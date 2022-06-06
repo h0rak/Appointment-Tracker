6d87e9a288bf115e4fce574014dbe581e64204c6
h0rak <trey.horak@gmail.com>
2022-06-06 15:48:02 -0700

    fourth commit

--- src/view_controller/LoginController.java
+++ src/view_controller/LoginController.java
@@ -1,9 +1,16 @@
 package view_controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,7 +24,14 @@ public class LoginController implements Initializable {
         passwordInput.clear();
     }
 
-    public void onActionLogin(ActionEvent actionEvent) {
+    public void onActionLogin(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view_controller/AppointmentScreen.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 900, 700);
+        stage.setTitle("Appointments");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
