1f5f939ac1b4f9b31429951e3974b3e3ce18f22d
h0rak <trey.horak@gmail.com>
2022-06-07 22:46:20 -0700

    seventh commit changes to appointment screen & changes to appointment controller

--- src/view_controller/AppointmentController.java
+++ src/view_controller/AppointmentController.java
@@ -1,4 +1,51 @@
 package view_controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
 public class AppointmentController {
+    @FXML
+    private RadioButton apptScreenRadioButton;
+
+    @FXML
+    private RadioButton contactScreenRadioButton;
+
+    @FXML
+    private RadioButton custScreenRadioButton;
+
+    @FXML
+    private RadioButton filterAllRadioButton;
+
+    @FXML
+    private RadioButton filterMonthRadioButton;
+
+    @FXML
+    private RadioButton filterWeekRadioButton;
+
+    @FXML
+    void onActionCustomerScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionFilterAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionFilterMonth(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionFilterWeek(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActoinContactScreen(ActionEvent event) {
+
+    }
 }
