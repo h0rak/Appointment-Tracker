d2bf284d338ef1ff0fcbe712708241e8d98b2dd3
h0rak <trey.horak@gmail.com>
2022-06-09 12:38:17 -0700

    11th commit fix contact schedule controller and screen

---
+++ src/view_controller/ContactScheduleController.java
@@ -0,0 +1,15 @@
+package view_controller;
+
+import javafx.event.ActionEvent;
+
+public class ContactScheduleController {
+
+    public void onActionCustomerScreen(ActionEvent actionEvent) {
+    }
+
+    public void onActionContactScreen(ActionEvent actionEvent) {
+    }
+
+    public void onActionViewSchedule(ActionEvent actionEvent) {
+    }
+}
