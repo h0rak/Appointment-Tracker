7be7ad3c9df5acda50ea67251f498489e49413a2
h0rak <trey.horak@gmail.com>
2022-06-12 01:03:40 -0500

    28 th commit added add/update appt screens & controllers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Locale;
 
 // have to be able to translate to french/english main or login controller?
 // Locale.setDefault(new Locale("fr"));
